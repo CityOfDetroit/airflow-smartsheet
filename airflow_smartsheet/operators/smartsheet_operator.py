78bac83234929a564d9ec713752841c90297b527
xyx0826 <xyx0826@hotmail.com>
2019-05-28 12:49:07 -0400

    Implement Postgres operator

--- airflow_smartsheet/operators/smartsheet_operator.py
+++ airflow_smartsheet/operators/smartsheet_operator.py
@@ -2,6 +2,7 @@
 
 import os
 import tempfile
+import subprocess
 import smartsheet
 
 from airflow.models import BaseOperator
@@ -9,8 +10,13 @@ from airflow.models import Variable
 from airflow.utils.decorators import apply_defaults
 from airflow.exceptions import AirflowException
 
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow_smartsheet.hooks.smartsheet_hook import SmartsheetHook
 from airflow_smartsheet.operators.enums import SmartsheetEnums
+
+
+DEFAULT_PG_CONN = "etl_postgres"
+DEFAULT_PG_SCHEMA = "public"
 
 
 class SmartsheetOperator(BaseOperator):
@@ -33,7 +39,7 @@ class SmartsheetOperator(BaseOperator):
     def execute(self, **kwargs):
         """Creates a Smartsheet API hook and establishes connection.
         """
-        
+
         self.smartsheet_hook = SmartsheetHook(self.token)
         self.smartsheet = self.smartsheet_hook.get_conn()
 
@@ -52,17 +58,17 @@ class SmartsheetToFileOperator(SmartsheetOperator):
                  *args, **kwargs):
         """Initializes a Smartsheet Get Sheet operator.
         This operator takes a Smartsheet sheet and saves it as a file.
-        
+
         Arguments:
             sheet_id {int} -- Sheet ID to fetch.
             sheet_type {str} -- Sheet type.
-        
+
         Keyword Arguments:
             paper_size {str} -- Optional paper size for PDF file type. (default: {None})
             output_dir {str} -- Optional output directory to override default OS temp path. (default: {None})
             with_json {bool} -- Whether to save a JSON dump alongside specified file type. (default: {False})
             no_overwrite {bool} -- Whether not to overwrite any file. (default: {False})
-        
+
         Raises:
             AirflowException: Raised when PDF file type is selected but paper size is unspecified.
         """
@@ -88,23 +94,23 @@ class SmartsheetToFileOperator(SmartsheetOperator):
             self.output_dir = output_dir
         else:
             self.output_dir = tempfile.gettempdir()
-        
+
         super().__init__(*args, **kwargs)
-    
+
     def _can_write(self, file_path):
         """Determines whether the current options allow (over)writing to the specified file path.
-        
+
         Arguments:
             file_path {str} -- File path to be written to.
-        
+
         Returns:
             bool -- Whether (over)writing to the specified path is allowed.
         """
         return not os.path.isfile(file_path) or not self.no_overwrite
-    
+
     def _ensure_paths(self):
         """Ensures all required output file paths are (over)writable.
-        
+
         Raises:
             AirflowException: Raised when unable to write to download file path.
             AirflowException: Raised when unable to write to JSON dump file path.
@@ -114,24 +120,24 @@ class SmartsheetToFileOperator(SmartsheetOperator):
             self.output_dir, str(self.sheet_id) + "." + self.sheet_type.name.lower())
         self.json_path = os.path.join(
             self.output_dir, str(self.sheet_id) + ".json")
-        
+
         if not self._can_write(self.file_path):
             # Cannot write to download path
             raise AirflowException(
                 f"Cannot write to download path {self.file_path} \
                 because the same-name file cannot be overwritten."
             )
-        
+
         if self.with_json and not self._can_write(self.json_path):
             # Cannot write to JSON path
             raise AirflowException(
                 f"Cannot write to JSON dump path {self.json_path} \
                 because the same-name file cannot be overwritten."
             )
-    
+
     def _ensure_removed(self, file_path):
         """Ensures the specified file path is empty.
-        
+
         Arguments:
             file_path {str} -- Path to file to be removed.
         """
@@ -145,12 +151,12 @@ class SmartsheetToFileOperator(SmartsheetOperator):
             # File does not exist; ignote
             pass
 
-    def execute(self, context):
+    def execute(self, context=None):
         """Fetches the specified sheet in the specified format.
-        
+
         Arguments:
             context {[type]} -- [description]
-        
+
         Raises:
             AirflowException: Raised when an unsupported sheet type is specified.
             AirflowException: Raised when the download returns an error.
@@ -189,7 +195,7 @@ class SmartsheetToFileOperator(SmartsheetOperator):
         # Get path to downloaded file
         download_path = os.path.join(
             downloaded_sheet.download_directory, downloaded_sheet.filename)
-        
+
         # Rename downloaded file to sheet ID
         self._ensure_removed(self.file_path)
         os.rename(download_path,  self.file_path)
@@ -200,21 +206,55 @@ class SmartsheetToFileOperator(SmartsheetOperator):
                 json_file.write(downloaded_sheet.to_json())
 
 
-class SmartsheetToPostgresOperator(SmartsheetOperator):
+class SmartsheetToPostgresOperator(SmartsheetToFileOperator):
     """The Smartsheet operator to save a sheet to database.
     """
 
     def __init__(
-        self,
-        sheet_id,
-        *args, **kwargs):
+            self,
+            sheet_id,
+            table_name,
+            postgres_conn_id=None,
+            postgres_schema=None,
+            *args, **kwargs):
         """Initializes a Smartsheet To Postgres operator.
         This operator takes a Smartsheet sheet and saves it to PostgreSQL.
-        
+
         Arguments:
             sheet_id {int} -- Sheet ID to fetch.
+            table_name {str} -- Name of the target table.
         """
-        
-        self.sheet_id = sheet_id
 
-        super().__init__(*args, **kwargs)
+        self.table_name = table_name
+        self.postgres_conn_id = postgres_conn_id
+        self.postgres_schema = postgres_schema
+
+        if postgres_conn_id is None:
+            self.postgres_conn_id = DEFAULT_PG_CONN
+
+        if postgres_schema is None:
+            self.postgres_schema = DEFAULT_PG_SCHEMA
+
+        super().__init__(
+            sheet_id,
+            SmartsheetEnums.SheetType.CSV
+        )
+
+    def _purge_table(self):
+        self.postgres.execute(f"TRUNCATE TABLE {self.table_name};")
+
+    def _copy_table(self):
+        self.postgres.copy_expert(
+            f"COPY {self.table_name} FROM {self.output_dir}/{self.sheet_id}.csv WITH (FORMAT csv, HEADER true);")
+
+    def execute(self, context):
+        # Initialize PostgreSQL hook
+        self.postgres = PostgresHook(
+            postgres_conn_id=self.postgres_conn_id,
+            schema=self.postgres_schema).get_sqlalchemy_engine()
+
+        # Fetch Smartsheet as file
+        super().execute()
+
+        self._purge_table()
+        self._copy_table()
